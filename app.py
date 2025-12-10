7d7b3619f8681b6e12275a4d75a20c8502aff2dc
dohonghg-collab <dohonghg@gmail.com>
2025-12-10 11:10:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -190,8 +190,8 @@ st.markdown("""
 # --- 4. GIAO DIỆN CHÍNH ---
 st.markdown("""
 <div class="main-header">
-    <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</p>
+    <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN  HỒNG (NLS)</h1>
+    <p>Tác giả: Đỗ Thị Hồng - Trường Tiểu học Nhữ Khê - ĐT: 0378077222</p>
 </div>
 """, unsafe_allow_html=True)
 
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Đỗ Thị Hồng - Trường Tiểu học Nhữ Khê- ĐT: 0378 077 222</div>", unsafe_allow_html=True)
